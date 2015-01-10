45f701d1ab053ca20205a55a83d0b8b8fee8ec82
jjlakin <j.lakin@gmail.com>
2015-01-10 17:59:15 +0000

    added game functionality and css

--- lib/rps_game.rb
+++ lib/rps_game.rb
@@ -1,9 +1,41 @@
 require 'sinatra/base'
+require_relative 'game'
+
 
 class RPSgame < Sinatra::Base
+
+	set :views, Proc.new { File.join(root, "..", "views") }
+  set :public_folder, 'public'
+
+	game = Game.new
+
   get '/' do
     'Hello RPSgame!'
+    erb :index
   end
+
+	post '/' do 
+		
+		erb :game
+		
+	end
+
+	get '/single_player' do
+    'Hello RPSgame!'
+    erb :game
+  end
+
+	post '/single_player' do 
+		
+
+		game.player1 = params[:selection].to_sym
+		game.player2 = game.computer_choice
+		@result = game.player1_result
+		erb :game
+		
+	end
+
+	
 
   # start the server if ruby file executed directly
   run! if app_file == $0
